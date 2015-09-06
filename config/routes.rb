d9b1821ce0c5df073ee017712b9306d91577fb03
Andrew <scytherswings@gmail.com>
2015-09-06 00:56:11 +0000

    Pointed routes root to services#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :services
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  resources :services
+  root 'services#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
