0a42d591433c13b2e86380db340eb497e69131b1
Andrew <scytherswings@gmail.com>
2015-09-06 01:58:18 +0000

    Adding tests for service validity. Defining what a service is

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,7 @@
 class Service < ActiveRecord::Base
+    validates :name, presence: true
+    validates :ip, presence: true
+    validates :dns_name, presence: true
+    validates :url, presence: true
     
 end
--- test/models/service_test.rb
+++ test/models/service_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class ServiceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @service = Service.new(id: 1, name: "Test Service", ip:"127.0.0.1", dns_name: "test", port: 8080, url: "http://127.0.0.1")
+  end
+  
+  test "should be valid" do
+    assert @service.valid?
+  end
+  
+  test "name should be present" do
+    @setvice.name = "     "
+    assert_not @service.valid?
+  end
+  
+  
 end
