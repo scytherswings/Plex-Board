7f69c6c888b440e4bfe43d86004e0c3d55bbbb92
Andrew <scytherswings@gmail.com>
2015-09-06 04:19:54 +0000

    started adding an integration test, then i realized i'm not there yet..

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  
+  # test "layout links" do
+  #   get root_path
+  #   assert_template 'services/index'
+  #   assert_select "a[href=?]", root_path, count: 2
+  # end
+end
