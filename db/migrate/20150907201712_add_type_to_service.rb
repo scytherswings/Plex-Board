a810624906d48c86c3f44f718494b6fb0a5b0b4a
Andrew <scytherswings@gmail.com>
2015-09-07 20:18:00 +0000

    Added type to a Service so we can do inheritance

---
+++ db/migrate/20150907201712_add_type_to_service.rb
@@ -0,0 +1,4 @@
+class AddTypeToService < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150907162028) do
+ActiveRecord::Schema.define(version: 20150907201712) do
 
   create_table "services", force: :cascade do |t|
     t.datetime "created_at", null: false
