b67942ec0c5ae32a66e5143da173a51ed1d7c2c0
Andrew <scytherswings@gmail.com>
2015-09-07 05:15:53 +0000

    fixed some routes

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -76,7 +76,7 @@ class ServicesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def service_params
-      params.require(:service).permit(:name, :ip, :dns_name, :port, :url, :status)
+      params.require(:service).permit(:name, :ip, :dns_name, :port, :url)
 
       
     end
