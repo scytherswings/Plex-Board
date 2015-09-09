3bc9c9696f92ee873f190d13500586e27d83d01e
Andrew <scytherswings@gmail.com>
2015-09-09 04:24:43 +0000

    Edited the config status and about pages

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -1,10 +1,13 @@
 class InfoController < ApplicationController
   def configuration
+    @services = Service.all
   end
 
   def about
+    @services = Service.all
   end
 
   def status
+    @services = Service.all
   end
 end
