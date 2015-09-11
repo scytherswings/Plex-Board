9d9dbb8aad0ecd63e7399efefdd5a579583d90a9
Andrew <scytherswings@gmail.com>
2015-09-11 04:25:09 +0000

    fixed some validation problems in the service model

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,14 +1,31 @@
 class Service < ActiveRecord::Base
     require 'resolv'
     validates :name, presence: true, uniqueness: true
-    validates :ip, format: { with: Resolv::IPv4::Regex }
+   
     validates :url, uniqueness: true, presence: true
-    validates_uniqueness_of :ip, scope: :port, if: "dns_name.nil?"
-    validates_uniqueness_of :dns_name, scope: :port, if: "ip.nil?"
+    validates_uniqueness_of :ip, scope: :port, if: :ip_addr_exists
+    validates_uniqueness_of :dns_name, scope: :port, if: :dns_name_exists
+    validates :ip, format: { with: Resolv::IPv4::Regex }, if: :ip_addr_exists
     validates_numericality_of :port
     after_initialize :init
     
     def init
         self.port ||=80
+    end
+    
+    def ip_addr_exists
+        if ip.nil? || ip.blank?
+            false
+        else
+            true
+        end
+    end
+    
+    def dns_name_exists
+        if dns_name.nil? || dns_name.blank?
+            false
+        else
+            true
+        end
     end
 end
