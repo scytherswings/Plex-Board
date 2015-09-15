aafc0bea35b37259e4e5a4c3fe2f1d4bab680b62
Andrew <scytherswings@gmail.com>
2015-09-15 16:55:28 +0000

    Will need to revisit the routes and services_controller to see if I can use the same create method everywhere

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
    
+   
+   #This fiddley stuff might not be needed if I can figure out how to make the 
+   #services controller be OK with extra stuff, I think it should work fine once 
+   #I figure that out
   resources :services do
   collection do
     post 'create_plex'
