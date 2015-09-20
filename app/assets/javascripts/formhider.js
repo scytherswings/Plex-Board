f0283119c30541da989ae9d392f668633649c4d2
Andrew <scytherswings@gmail.com>
2015-09-20 05:21:56 +0000

    Okay, maybe now the form hide logic works

--- app/assets/javascripts/formhider.js
+++ app/assets/javascripts/formhider.js
@@ -1,22 +1,35 @@
 
 // $(document).ready(function(){
 $(function hideStuff(){
-    $('[id=service_service_type]').val( "Plex");
-
-    $(function() {
+  $('[id=service_service_type]').val( "Generic Service");
+      $(function() {
       var serviceType = localStorage.getItem("serviceType");
       if(serviceType != null) {
-          $("select[id=service_service_type]").val(serviceType);
+        $("select[id=service_service_type]").val(serviceType);
+          var value = $('#service_service_type').children("option").filter(":selected").text()
+          if (value == "Generic Service"){
+            $("#login_div").hide();
+            $("#api_div").hide();
+          }
+          else {
+            $("#login_div").show();
+            $("#api_div").show();
+            $("#default_info_div").show();
+            $("#url_div").show();
+          }
       }
-
+      
       $("select[id=service_service_type]").on("change", function() {
-          localStorage.setItem("serviceType", $(this).val());
+        localStorage.setItem("serviceType", $(this).val());
       });
     });
 
 
-  $( '#wrapper' ).on( 'load', 'select', function(){
-  var value = $('#service_service_type').children("option").filter(":selected").text()
+  $( '#wrapper' ).on( 'click', 'select', function(){
+    
+    
+
+    var value = $('#service_service_type').children("option").filter(":selected").text()
     if (value == "Generic Service"){
       $("#login_div").hide();
       $("#api_div").hide();
