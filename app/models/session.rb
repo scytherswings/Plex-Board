5455843bd7ff6cb64cd46756951f3e5a1e4226d1
Andrew <scytherswings@gmail.com>
2015-10-05 00:48:21 +0000

    Tired of fighting with images, shouldnt have to roll my own but carrierwave doesnt like what im trying to do to it

--- app/models/session.rb
+++ app/models/session.rb
@@ -8,6 +8,8 @@ class Session < ActiveRecord::Base
   # mount_uploader :image, ImageUploader
   before_destroy :delete_thumbnail
   
+  @@images_dir = "public/images"
+  
   def init
     self.thumb_url = "http://placehold.it/400x592"
     self.image = "http://placehold.it/400x592"
@@ -16,7 +18,8 @@ class Session < ActiveRecord::Base
   def delete_thumbnail
     if self.image != "http://placehold.it/400x592"
       begin
-        FileUtils.rm(image)
+        FileUtils.rm("#{@@images_dir}/#{image}")
+        logger.debug("Deleted #{@@images_dir}/#{image}")
       rescue => error
       logger.debug(error)
       end
@@ -24,38 +27,20 @@ class Session < ActiveRecord::Base
   end
   
   def get_plex_now_playing_img()
-    
-    
+  
     # self.remote_image_url = "#{self.connection_string}#{self.thumb_url}"
-    # self.save
-
-    # resource = RestClient::Resource.new("#{connection_string}#{thumb_url}",
-    #   verify_ssl: OpenSSL::SSL::VERIFY_NONE)
-    # self.image_data_uri = resource.get(:"X-Plex-Token" => self.service_token, :accept => "image/jpeg")
-    # self.save!
-
-    # self.image_data_uri = open("#{self.connection_string}#{self.thumb_url}", "X-Plex-Token" => self.service_token, ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE).read
-    # self.save!
-
-
-    # self.update(self.remote_image_url => "#{self.connection_string}#{self.thumb_url}")
-
-    # resource = RestClient::Resource.new("#{connection_string}#{thumb_url}",
-    #   verify_ssl: OpenSSL::SSL::VERIFY_NONE)
-    # uploader = ImageUploader.new
-    # uploader.store!(File.open(resource.get(:"X-Plex-Token" => self.service_token,
-    #   :accept => "image/jpeg"), "wb"))
-    # uploader.store!(open("#{self.connection_string}#{self.thumb_url}", "X-Plex-Token" => self.service_token, ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE))
-    if !File.directory?("public/images")
-      FileUtils::mkdir_p "public/images/"
-      File.open("public/images/#{self.id}.jpeg", 'wb') do |f|
+    # self.save!  
+  
+    if !File.directory?(@@images_dir)
+      FileUtils::mkdir_p @@images_dir
+      File.open("#{@@images_dir}/#{self.id}.jpeg", 'wb') do |f|
         f.write open("#{self.connection_string}#{self.thumb_url}", 
         "X-Plex-Token" => self.service_token, "Accept" => "image/jpeg",
         ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE).read
       end
       self.image = "#{self.id}.jpeg"
     else
-      File.open("public/images/#{self.id}.jpeg", 'wb') do |f|
+      File.open("#{@@images_dir}/#{self.id}.jpeg", 'wb') do |f|
         f.write open("#{self.connection_string}#{self.thumb_url}", 
         "X-Plex-Token" => self.service_token, "Accept" => "image/jpeg",
         ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE).read
@@ -63,25 +48,6 @@ class Session < ActiveRecord::Base
       self.image = "#{self.id}.jpeg"
     end
     
-    # tempfile = open("#{self.connection_string}#{self.thumb_url}", "X-Plex-Token" => self.service_token, ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE)
-    # logger.debug(tempfile.class)
-    # uploader = ImageUploader.new
-
-    # uploader.store! tempfile
-
-
-    # uploader = ImageUploader.new
-
-    # uploader.store! open("#{self.connection_string}#{self.thumb_url}",
-    #   "X-Plex-Token" => self.service_token, "Accept" => "image/jpeg",
-    #   ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE)
-
-
-
-    # resource = RestClient::Resource.new("#{connection_string}#{thumb_url}",
-    #   verify_ssl: OpenSSL::SSL::VERIFY_NONE)
-    # logger.debug(resource.get(:"X-Plex-Token" => self.service_token,
-    #   :accept => "image/jpeg"))
 
   end
 
