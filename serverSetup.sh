26c9d5a89761016e3f3c85111ce57f3d0630ce7e
Andrew <scytherswings@gmail.com>
2015-10-17 04:04:12 +0000

    removed tab

--- serverSetup.sh
+++ serverSetup.sh
@@ -2,5 +2,5 @@
 SECRETS="config/secrets_test.yml"
 SECRET=`rake secret`
 rm $SECRETS
-echo -e "production:\n\tsecret_key_base:" $SECRET >> $SECRETS
+echo -e "production:\n  secret_key_base:" $SECRET >> $SECRETS
 
