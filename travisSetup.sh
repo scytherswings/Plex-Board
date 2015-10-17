de18731a9ea224322b5e737410744cdabb5f0cc4
Andrew <scytherswings@gmail.com>
2015-10-17 05:05:50 +0000

    travis fix

--- travisSetup.sh
+++ travisSetup.sh
@@ -8,6 +8,5 @@ echo -e "test:\n  secret_key_base:" $BOGUS "\n" >> $SECRETS
 echo -e "production:\n  secret_key_base:" $SECRET >> $SECRETS
 `rm db/*.sqlite3`
 `rake db:create`
-`rake db:schema:load`
 `rake db:migrate`
 `rake assets:precompile`
