9f91f1a0555950493ad67d9c73e3ed3794e1bc0f
Andrew <scytherswings@gmail.com>
2015-10-17 05:01:38 +0000

    updated travis thing

---
+++ travisSetup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+SECRETS="config/secrets.yml"
+SECRET=`rake secret`
+BOGUS="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+`rm $SECRETS`
+echo -e "development:\n  secret_key_base:" $BOGUS "\n" >> $SECRETS
+echo -e "test:\n  secret_key_base:" $BOGUS "\n" >> $SECRETS
+echo -e "production:\n  secret_key_base:" $SECRET >> $SECRETS
+echo -e "RAILS_ENV=production"
+`rm db/*.sqlite3`
+`rake db:create`
+`rake db:schema:load`
+`rake db:migrate`
+`rake assets:precompile`
