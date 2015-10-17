5597a62dab9561a80c832bf5aad83d90584cd774
Andrew <scytherswings@gmail.com>
2015-10-17 05:02:58 +0000

    travis fix

--- travisSetup.sh
+++ travisSetup.sh
@@ -6,7 +6,6 @@ BOGUS="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 echo -e "development:\n  secret_key_base:" $BOGUS "\n" >> $SECRETS
 echo -e "test:\n  secret_key_base:" $BOGUS "\n" >> $SECRETS
 echo -e "production:\n  secret_key_base:" $SECRET >> $SECRETS
-echo -e "RAILS_ENV=production"
 `rm db/*.sqlite3`
 `rake db:create`
 `rake db:schema:load`
