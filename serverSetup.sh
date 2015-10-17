c404635d9feee8ee3b7e9527ca69401528823e2a
Andrew <scytherswings@gmail.com>
2015-10-17 04:13:30 +0000

    DB operations

--- serverSetup.sh
+++ serverSetup.sh
@@ -3,5 +3,7 @@ SECRETS="config/secrets.yml"
 SECRET=`rake secret`
 rm $SECRETS
 echo -e "production:\n  secret_key_base:" $SECRET >> $SECRETS
+RAILS_ENV=production
+rake db:create
 rake db:migrate
 
