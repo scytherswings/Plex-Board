09a93988850a8b46b732d5c181a1203a5a2921c8
Andrew <scytherswings@gmail.com>
2015-10-17 15:34:35 +0000

    commented out remove service tests and updated serverSetup.sh

--- serverSetup.sh
+++ serverSetup.sh
@@ -2,13 +2,17 @@
 SECRETS="config/secrets.yml"
 SECRET=`rake secret`
 BOGUS="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+echo "Removing old secrets.yml file if it exists"
 `rm $SECRETS`
 echo -e "development:\n  secret_key_base:" $BOGUS "\n" >> $SECRETS
 echo -e "test:\n  secret_key_base:" $BOGUS "\n" >> $SECRETS
 echo -e "production:\n  secret_key_base:" $SECRET >> $SECRETS
 echo -e "RAILS_ENV=production"
+echo "Removing old databases if they exist"
 `rm db/*.sqlite3`
+echo "Creating and setting up new database for production"
 `rake db:create RAILS_ENV=production`
 `rake db:schema:load RAILS_ENV=production`
 `rake db:migrate RAILS_ENV=production`
 `rake assets:precompile RAILS_ENV=production`
+echo "Finished. Run ./runServer.sh to start the server! It runs on port 3000 by default."
