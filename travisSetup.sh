2e0a8b6343243f73294c84aa8e716b811f8daf4c
Andrew <scytherswings@gmail.com>
2015-10-17 17:45:01 +0000

    travis

--- travisSetup.sh
+++ travisSetup.sh
@@ -6,6 +6,6 @@ BOGUS="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 echo -e "development:\n  secret_key_base:" $BOGUS "\n" >> $SECRETS
 echo -e "test:\n  secret_key_base:" $BOGUS "\n" >> $SECRETS
 echo -e "production:\n  secret_key_base:" $SECRET >> $SECRETS
-`rm db/*.sqlite3`
-`rake db:create`
-`rake db:migrate`
+# `rm db/*.sqlite3`
+# `rake db:create`
+# `rake db:migrate`
