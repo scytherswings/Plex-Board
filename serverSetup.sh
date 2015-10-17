5256575c47a63759920836b4cbe917de7d3c20d5
Andrew <scytherswings@gmail.com>
2015-10-17 03:57:47 +0000

    Made auto secrets script

--- serverSetup.sh
+++ serverSetup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-rm config/secrets.yml
-echo "production: secret_key_base: " >> config/secrets.yml
-rake secret >> config/secrets.yml
+SECRETS="config/secrets.yml"
+SECRET=`rake secret`
+rm $SECRETS
+echo -e "production:" >> $SECRETS
+echo -e "\tsecret_key_base:" $SECRET >> $SECRETS
+
