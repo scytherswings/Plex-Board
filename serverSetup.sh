b043d5f1c8665a9682d26db0bdfdb6c3c9a04175
Andrew <scytherswings@gmail.com>
2015-10-17 04:43:02 +0000

    asdf

--- serverSetup.sh
+++ serverSetup.sh
@@ -2,13 +2,13 @@
 SECRETS="config/secrets.yml"
 SECRET=`rake secret`
 BOGUS="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-rm $SECRETS
+`rm $SECRETS`
 echo -e "development:\n  secret_key_base:" $BOGUS "\n" >> $SECRETS
 echo -e "test:\n  secret_key_base:" $BOGUS "\n" >> $SECRETS
 echo -e "production:\n  secret_key_base:" $SECRET >> $SECRETS
 echo -e "RAILS_ENV=production"
-rm db/*.sqlite3
-rake db:create db:schema:load
-rake db:migrate
-rake assets:precompile
+`rm db/*.sqlite3`
+`rake db:create db:schema:load`
+`rake db:migrate`
+`rake assets:precompile`
 
