2f401aa17b17e176b82371d4283bcb8aede4b61e
scytherswings <scytherswings@gmail.com>
2015-12-05 17:34:17 -0500

    Updated tests to look at page titles

--- test/controllers/services_controller_test.rb
+++ test/controllers/services_controller_test.rb
@@ -1,57 +1,70 @@
 require 'test_helper'
 class ServicesControllerTest < ActionController::TestCase
   
-  test "should get index" do
+
+  test 'should get index' do
     get :index
     assert_response :success
     assert_not_nil assigns(:services)
+    assert_select 'title', 'Plex-Board'
   end
-  test "should get all_services" do
+  
+  test 'should get all_services' do
     get :all_services
     assert_response :success
     assert_not_nil assigns(:services)
+    assert_select 'title', 'Plex-Board | All Services'
   end
 
-  test "should get new" do
+  test 'should get choose_service_type' do
+    get :choose_service_type
+    assert_response :success
+    assert_select 'title', 'Plex-Board | Choose Service Type'
+  end
+  
+  test 'should get new' do
     get :new
     assert_response :success
+    assert_select 'title', 'Plex-Board | New Service'
   end
 
-  test "should create service" do
+  test 'should create service' do
     assert_difference('Service.count') do
-      post :create, service: { name: "test_create", ip: "172.111.3.1", dns_name: "test_create", url: "test_create"}
+      post :create, service: { name: 'test_create', ip: '172.111.3.1', dns_name: 'test_create', url: 'test_create'}
     end
 
     assert_redirected_to service_path(assigns(:service))
   end
 
-  test "should show service" do
+  test 'should show service' do
     get :show, id: @generic_service_one
     assert_response :success
+    assert_select 'title', "Plex-Board | #{@generic_service_one.name}"
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get :edit, id: @generic_service_one
     assert_response :success
+    assert_select 'title', "Plex-Board | Edit #{@generic_service_one.name}"
   end
 
-  test "should update service" do
+  test 'should update service' do
 
-    patch :update, id: @generic_service_one, service: { name: "test2", ip: "172.123.1.1", dns_name: "test", url: "test" }
+    patch :update, id: @generic_service_one, service: { name: 'test2', ip: '172.123.1.1', dns_name: 'test', url: 'test' }
     assert_redirected_to service_path(assigns(:service))
   end
 
-  test "should destroy service" do
+  test 'should destroy service' do
     assert_difference('Service.count', -1) do
       assert delete :destroy, id: @generic_service_one
     end
     assert_redirected_to root_url
   end
 
-  # test "Bad plex service port wont break page load" do
+  # test 'Bad plex service port wont break page load' do
   #   get :index
   #   assert_response :success
-  #   assert_requested(:get, "https://plex1:32400/status/sessions")
+  #   assert_requested(:get, 'https://plex1:32400/status/sessions')
   #   @plex_service_one.update(port: 32401)
   #   get :index
 
@@ -59,7 +72,7 @@ class ServicesControllerTest < ActionController::TestCase
   # end
 
   # This might take some tinkering
-  # test "should show service as online" do
+  # test 'should show service as online' do
 
   # end
 
