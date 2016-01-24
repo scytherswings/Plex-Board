92a0b52e5cacacafd1d32e572a1ddf4eb3e1cdfb
scytherswings <scytherswings@gmail.com>
2016-01-24 01:51:59 -0500

    travis always has problems with wiriting files and shit. Added a 1s wait before checking for the image file

--- test/models/plex_object_test.rb
+++ test/models/plex_object_test.rb
@@ -16,19 +16,21 @@ class PlexObjectTest < ActiveSupport::TestCase
 
   test 'plex_object should successfully retrieve image' do
     assert @plex_object_session_1.delete_thumbnail, 'Deleting thumbnail failed'
-    assert_not File.file?(Rails.root.join 'test/test_images', (@plex_object_session_1.id.to_i.to_s + '.jpeg')),
+    assert_not File.file?(Rails.root.join 'test/test_images', (@plex_object_session_1.id.to_s + '.jpeg')),
                'Image file should not be present'
     assert_not_nil @plex_object_session_1.get_img, 'Image file was not retrieved'
-    assert File.file?(Rails.root.join 'test/test_images', (@plex_object_session_1.id.to_i.to_s + '.jpeg')),
+    sleep(1) #strategic wait
+    assert File.file?(Rails.root.join 'test/test_images', (@plex_object_session_1.id.to_s + '.jpeg')),
            'Image file was not found'
   end
 
   test 'destroying a plex_object will delete the associated image' do
     assert_not_nil @plex_object_session_1.get_img, 'Image file was not retrieved'
-    assert File.file?(Rails.root.join 'test/test_images', (@plex_object_session_1.id.to_i.to_s + '.jpeg')),
+    assert File.file?(Rails.root.join 'test/test_images', (@plex_object_session_1.id.to_s + '.jpeg')),
            'Image file was not found'
     assert @plex_object_session_1.destroy, 'Destroying the session failed'
-    assert_not File.file?(Rails.root.join 'test/test_images', (@plex_object_session_1.id.to_i.to_s + '.jpeg')),
+    sleep(1) #strategic wait
+    assert_not File.file?(Rails.root.join 'test/test_images', (@plex_object_session_1.id.to_s + '.jpeg')),
                'The image file was not deleted'
   end
 end
