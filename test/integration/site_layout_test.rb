5fb4c0ace060f7acc36038c0bc7de32f45aeee78
scytherswings <scytherswings@gmail.com>
2016-01-25 01:06:05 -0500

    Added capybara screenshots. Going to work on getting javascript running in the tests

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,25 +2,21 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
   keywords = %w(Plex-Board Services Config Status About)
-  setup do
-    Capybara.current_driver = Capybara.javascript_driver # :selenium by default
 
-  end
-
-  def teardown
-    super
-    Capybara.reset_sessions!
-    Capybara.use_default_driver
-  end
+  # def teardown
+  #   super
+  #   Capybara.reset_sessions!
+  #   Capybara.use_default_driver
+  # end
 
 
   test 'check index for all expected links' do
-    # keywords = %w(Plex-Board Services Config Status About)
     visit '/'
     keywords.each do |word|
       assert page.has_content?(word), "#{word} was missing from \"/\""
     end
     assert page.has_title?('Plex-Board'), '"Plex-Board" was not the title for "/"'
+    Capybara::Screenshot.screenshot_and_save_page
   end
 
   # test 'hit index and check for carousel item' do
