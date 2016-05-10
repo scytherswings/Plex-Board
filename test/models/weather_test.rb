e11ed2ad5e04c91f911253fab097307d1f4a269c
scytherswings <scytherswings@gmail.com>
2016-05-10 00:19:52 -0400

    Trying to trigger code usage in tests

--- test/models/weather_test.rb
+++ test/models/weather_test.rb
@@ -9,6 +9,7 @@ class WeatherTest < ActiveSupport::TestCase
   test 'a provided address will be converted to lat and long' do
     weather = Fabricate.build(:weather)
     weather.address = '2300 Traverwood Dr, Ann Arbor, MI 48105'
+    weather.save!
     assert weather.valid?, "Weather with address: \"#{weather.address}\" did not get a lat and long."
   end
 
@@ -16,6 +17,7 @@ class WeatherTest < ActiveSupport::TestCase
     skip 'needs further validation'
     weather = Fabricate.build(:weather)
     weather.latitude = 35.2341
+    weather.save!
     assert_not weather.valid?, 'Weather should not be valid if only supplied latitude'
   end
 end
