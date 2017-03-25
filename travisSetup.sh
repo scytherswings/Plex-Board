dbce319b15c5786b565153408e658f487c63fd8d
scytherswings <scytherswings@gmail.com>
2017-03-25 02:44:41 -0400

    bundle exec all the things

--- travisSetup.sh
+++ travisSetup.sh
@@ -7,5 +7,5 @@ echo -e "development:\n  secret_key_base: " $BOGUS "\n" >> $SECRETS
 echo -e "test:\n  secret_key_base: " $BOGUS "\n" >> $SECRETS
 echo -e "production:\n secret_key_base: " $SECRET >> $SECRETS
 # rm db/*sqlite3
-rake db:create
-rake db:migrate
+bundle exec rake db:create
+bundle exec rake db:migrate
