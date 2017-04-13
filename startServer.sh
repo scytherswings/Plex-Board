9c9095470f1eff827db5ad6ba8262ec4d2f34fc5
scytherswings <scytherswings@gmail.com>
2017-04-12 23:58:39 -0400

    dont use exit 0. just wrap everything in a big nasty if

--- startServer.sh
+++ startServer.sh
@@ -2,7 +2,6 @@
 
 cd "$(dirname "$0")"
 
-
 case "$(uname -s)" in
 
    Darwin)
@@ -25,27 +24,25 @@ esac
 if [[ "$OS" == "Windows" ]]; then
     printf "\nNot creating pid files etc. since we're on Windows. Starting puma.\n"
     bundle exec puma -e production -C config/puma.rb config.ru
-    exit 0
-fi
-
-
-HOME_RVM=$HOME/.rvm/scripts/rvm
-ROOT_RVM="/usr/local/rvm/scripts/rvm"
-# Load RVM into a shell session *as a function*
-if [[ -s ${HOME_RVM} ]] ; then
-  # First try to load from a user install
-  source ${HOME_RVM} \
-  && printf "\nRVM successfully loaded from $HOME_RVM\n"
-
-elif [[ -s ${ROOT_RVM} ]] ; then
-  # Then try to load from a root install
-  source ${ROOT_RVM} \
-  && printf "\nRVM successfully loaded from $ROOT_RVM\n"
 else
-  printf "\nWARNING: An RVM installation was not found. Did you follow the instructions correctly? Attempting to use system Ruby...\n"
+  HOME_RVM=$HOME/.rvm/scripts/rvm
+  ROOT_RVM="/usr/local/rvm/scripts/rvm"
+  # Load RVM into a shell session *as a function*
+  if [[ -s ${HOME_RVM} ]] ; then
+    # First try to load from a user install
+    source ${HOME_RVM} \
+    && printf "\nRVM successfully loaded from $HOME_RVM\n"
+
+  elif [[ -s ${ROOT_RVM} ]] ; then
+    # Then try to load from a root install
+    source ${ROOT_RVM} \
+    && printf "\nRVM successfully loaded from $ROOT_RVM\n"
+  else
+    printf "\nWARNING: An RVM installation was not found. Did you follow the instructions correctly? Attempting to use system Ruby...\n"
+  fi
+
+  printf "\nIf you run into any issues, check the error logs in: log/production.stderr.log, log/production.stdout.log, and log/production.log\n\n"
+
+  mkdir -p tmp/pids && mkdir -p tmp/sockets && touch tmp/pids/puma.pid && touch tmp/sockets/puma.socket && exec bundle exec puma -e production -C config/puma.rb config.ru
+
 fi
-
-printf "\nIf you run into any issues, check the error logs in: log/production.stderr.log, log/production.stdout.log, and log/production.log\n\n"
-
-mkdir -p tmp/pids && mkdir -p tmp/sockets && touch tmp/pids/puma.pid && touch tmp/sockets/puma.socket && exec bundle exec puma -e production -C config/puma.rb config.ru
-
--- stopServer.sh
+++ stopServer.sh
@@ -22,21 +22,21 @@ esac
 if [[ "$OS" == "Windows" ]]; then
     printf "\nNo puma daemon to kill since we're on Windows.\n"
     exit 0
-fi
-
-cd "$(dirname "$0")"
-PIDFILE="tmp/pids/puma.pid"
-STATEFILE="tmp/pids/puma.state"
-
-if [ -e ${PIDFILE} ]
-then
-  PUMA_PID="$(cat ${PIDFILE})"
-
-  printf "Attempting to kill puma with PID: $PUMA_PID" \
-  && kill -9 ${PUMA_PID} \
-  && printf "\nServer stopped.\n" \
-  && rm ${PIDFILE} \
-  && rm ${STATEFILE}
 else
-  printf "It seems Puma isn't running right now. Nothing to kill.\n"
+  cd "$(dirname "$0")"
+  PIDFILE="tmp/pids/puma.pid"
+  STATEFILE="tmp/pids/puma.state"
+
+  if [ -e ${PIDFILE} ]
+  then
+    PUMA_PID="$(cat ${PIDFILE})"
+
+    printf "Attempting to kill puma with PID: $PUMA_PID" \
+    && kill -9 ${PUMA_PID} \
+    && printf "\nServer stopped.\n" \
+    && rm ${PIDFILE} \
+    && rm ${STATEFILE}
+  else
+    printf "It seems Puma isn't running right now. Nothing to kill.\n"
+  fi
 fi
