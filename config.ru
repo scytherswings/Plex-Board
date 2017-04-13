1469d9a0f76c8c83187a2bd610b4d05a0dd2fae1
scytherswings <scytherswings@gmail.com>
2017-04-13 09:09:30 -0400

    fixed using relative_root without setting use_reverse_proxy to true

--- config.ru
+++ config.ru
@@ -2,7 +2,7 @@
 
 require ::File.expand_path('../config/environment', __FILE__)
 # run Rails.application
-config_file = 'server_config.yml'
+config_file = Rails.root.join 'server_config.yml'
 if !File.exist?(config_file)
   error_string = "Plex-Board was started without a #{config_file}. You should not run without it. RTFM."
   puts error_string
@@ -10,7 +10,7 @@ if !File.exist?(config_file)
 else
   relative_root = YAML.load_file(config_file)['relative_root']
 end
-relative_root ||= '/'
-map relative_root do
+
+map relative_root ||= '/' do
   run Rails.application
 end
--- config/initializers/server_config.rb
+++ config/initializers/server_config.rb
@@ -1,12 +1,15 @@
 config_file = Rails.root.join('server_config.yml')
 
 if config_file.exist?
-  server_config = YAML.load_file(config_file)
-
-  relative_root = server_config['relative_root']
+  relative_root = YAML.load_file(config_file)['relative_root']
 
   unless relative_root.blank?
     Rails.logger.info("Setting relative_root to: #{relative_root}")
+    Rails.application.config.relative_url_root = relative_root
+    Rails.application.config.action_controller.relative_url_root = relative_root
     ENV['RAILS_RELATIVE_URL_ROOT'] = relative_root
+    ENV['ROOT_URL'] = relative_root
   end
+else
+  raise StandardError('The config file: server_config.yml was not found. RTFM.')
 end
