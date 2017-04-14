43dc7a6c79a2eef08635e35a7a6c9eee64cf8f35
scytherswings <scytherswings@gmail.com>
2017-04-14 16:32:21 -0400

    don't assume RAILS_ENV is set

--- config/puma.rb
+++ config/puma.rb
@@ -36,7 +36,7 @@ else
     bind web_host_binding
   end
 
-  if ENV['NO_DAEMONIZE'] || ENV['RAILS_ENV'] != 'production'
+  if ENV['NO_DAEMONIZE'] || ENV['RAILS_ENV'] == 'development' || ENV['RAILS_ENV'] == 'test'
     puts 'Detected NO_DAEMONIZE. Not daemonizing.'
     daemonize false
   else
